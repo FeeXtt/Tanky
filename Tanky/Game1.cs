5ee3e6f8ed54c296ea5ecde170edac40eb884335
Stanislav Cinkl <stanislav.cinkl@student.smspmb.cz>
2024-11-05 23:52:36 +0100

    je to rozbitý

--- Tanky/Game1.cs
+++ Tanky/Game1.cs
@@ -18,12 +18,30 @@ namespace Tanky
 
         private Vector2 playerPosition;
         private float playerSpeed = 100f;
-        private float playerRotation;
+        private float playerRotation = 0f;
+        private float playerRotationSpeed = 2f;
+        private int playerHealth = 6;
         
         private Texture2D bulletTexture;
         private List<Vector2> bulletPositions;
         private List<Vector2> bulletDirections;
         private float bulletSpeed = 200f;
+        
+        
+        private Texture2D enemyBulletTexture;
+        private List<Vector2> enemyBulletPositions;
+        private List<Vector2> enemyBulletDirections;
+        private float enemyBulletSpeed = 170f;
+        private float enemyCooldown = 3f;
+        private float enemyShootTimer = 0f;
+        
+        private Texture2D enemyTankTexture;
+        private Vector2 enemyTankPosition;
+        private float enemyTankSpeed = 55f;
+        private float enemyTankRotation = 0f;
+        private int enemyHealth = 6;
+        private bool enemyIsAlive = true;
+        private Random random;
 
 
 
@@ -38,9 +56,14 @@ namespace Tanky
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            playerPosition = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
-            bulletPositions = new List<Vector2>(); 
+            playerPosition = new Vector2(100, 100);
+            enemyTankPosition = new Vector2(300, 300);
+            bulletPositions = new List<Vector2>();
             bulletDirections = new List<Vector2>();
+            enemyBulletPositions = new List<Vector2>();
+            enemyBulletDirections = new List<Vector2>();
+            random = new Random();
+            
             base.Initialize();
         }
 
@@ -52,6 +75,8 @@ namespace Tanky
 
             _texture = Content.Load<Texture2D>("tank");
             bulletTexture = Content.Load<Texture2D>("bullet");
+            enemyTankTexture = Content.Load<Texture2D>("enemy");
+            enemyBulletTexture = Content.Load<Texture2D>("bullet");
 
             
             
@@ -62,6 +87,7 @@ namespace Tanky
             var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
             var state = Keyboard.GetState();
             var mouseState = Mouse.GetState();
+            var viewport = _graphics.GraphicsDevice.Viewport;
 
             if (state.IsKeyDown(Keys.W))
                 playerPosition.Y -= playerSpeed * deltaTime;
@@ -86,14 +112,78 @@ namespace Tanky
             
             if (mouseState.LeftButton == ButtonState.Pressed)
             {
-                var bulletDirection = Vector2.Normalize(directionToMouse);
-                bulletPositions.Add(playerPosition);
-                bulletDirections.Add(bulletDirection);
+                Vector2 bulletPosition = playerPosition;
+                Vector2 mousePosition = new Vector2(mouseState.X, mouseState.Y);
+                Vector2 direction = Vector2.Normalize(mousePosition - bulletPosition);
+
+                bulletPositions.Add(bulletPosition);
+                bulletDirections.Add(direction);
+                
+                
             }
             
             for (int i = 0; i < bulletPositions.Count; i++)
             {
                 bulletPositions[i] += bulletDirections[i] * bulletSpeed * deltaTime;
+            }
+
+            if (enemyIsAlive)
+            {
+                Vector2 directionToTank = playerPosition - enemyTankPosition;
+                if (directionToTank.Length() > 10f) 
+                {
+                    directionToTank.Normalize();
+                    enemyTankPosition += directionToTank * enemyTankSpeed * deltaTime;
+                    enemyTankRotation = (float)Math.Atan2(directionToTank.Y, directionToTank.X);
+                }
+
+                
+                enemyShootTimer += deltaTime;
+                if (enemyShootTimer >= enemyCooldown)
+                {
+                    Vector2 enemyBulletDirection = Vector2.Normalize(playerPosition - enemyTankPosition);
+                    enemyBulletPositions.Add(enemyTankPosition);
+                    enemyBulletDirections.Add(enemyBulletDirection);
+                    enemyShootTimer = 0f;
+                }
+            }
+            
+            for (int i = 0; i < enemyBulletPositions.Count; i++)
+            {
+                enemyBulletPositions[i] += enemyBulletDirections[i] * enemyBulletSpeed * deltaTime;
+            }
+
+
+            for (int i = 0; i < bulletPositions.Count; i++)
+            {
+                if (enemyIsAlive && Vector2.Distance(bulletPositions[i], enemyTankPosition) < 20f)
+                {
+                    bulletPositions.RemoveAt(i);
+                    bulletDirections.RemoveAt(i);
+                    enemyHealth--;
+                    if (enemyHealth <= 0)
+                    {
+                        enemyIsAlive = false;
+                    }
+                    break;
+                }
+            }
+            
+            if (Vector2.Distance(playerPosition, enemyTankPosition) < 30f && enemyIsAlive)
+            {
+                playerHealth--;
+                playerPosition = new Vector2(100, 100);
+            }
+
+            for (int i = 0; i < enemyBulletPositions.Count; i++)
+            {
+                if (Vector2.Distance(enemyBulletPositions[i], playerPosition) < 10f)
+                {
+                    enemyBulletPositions.RemoveAt(i);
+                    enemyBulletDirections.RemoveAt(i);
+                    playerHealth--;
+                    break;
+                }
             }
             base.Update(gameTime);
         }
@@ -118,6 +208,12 @@ namespace Tanky
                 SpriteEffects.None,
                 0f);
             
+            if (enemyIsAlive)
+            {
+                _spriteBatch.Draw(enemyTankTexture, enemyTankPosition, null, Color.White, enemyTankRotation,
+                    new Vector2(enemyTankTexture.Width / 2, enemyTankTexture.Height / 2), 1f, SpriteEffects.None, 0f);
+            }
+            
             foreach (var bulletPosition in bulletPositions)
             {
                 _spriteBatch.Draw(
@@ -132,6 +228,12 @@ namespace Tanky
                     SpriteEffects.None,
                     0f);
             }
+            foreach (var enemyBullet in enemyBulletPositions )
+            {
+                _spriteBatch.Draw(bulletTexture, enemyBullet, Color.Yellow);
+            }
+
+            
 
 
 
